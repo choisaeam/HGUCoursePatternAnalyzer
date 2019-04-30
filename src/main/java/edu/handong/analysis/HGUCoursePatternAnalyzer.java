165b143116497db488ae6d7277945b1b05e32ac8
choisaeam <choi.saeam@Gmail.com>
2019-04-30 22:55:33 +0900

    edit Analyzer.java

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,35 +1,102 @@
 package edu.handong.analysis;
 
-import edu.handong.analysis.datamodel.*;
+import edu.handong.analysis.datamodel.Course;
+import edu.handong.analysis.datamodel.Student;
 
 public class HGUCoursePatternAnalyzer {
 	
-	private int numOfStudents; // 총 학생의 수
-	private int numOfCourses; // 총 수업의 수
-	private Student[] students; // lines에서 학생 목록을 뽑아서 저장
-	private Course[] courses; // lines에서 course 목록을 뽑아서 저장
+	String[] lines = {	"1999-1, JC Nam, Java Programming",
+						"1999-2, JC Nam, Programming Language Theory",
+						"1999-1, JC Nam, Data Structures",
+						"2001-1, JC Nam, Database Systems",
+						"2018-1, SB Lim, Java Programming",
+						"2018-2, SB Lim, Programming Language Theory",
+						"2019-1, SB Lim, Data Structures",
+						"2019-1, SB Lim, Algorithm Analysis",
+						"2018-1, SJ Kim, Java Programming",
+						"2018-2, SJ Kim, Programming Language Theory",
+						"2019-1, SJ Kim, Logic Design",
+						"2019-1, SJ Kim, Algorithm Analysis",
+						};
 
-	public static void main(String[] args) {
-		HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
-		analyzer.run(args);
-	}
-
-	private void run(String [] args) {
+	int numOfStudents;
+	int numOfCourses;
+	Student[] students;
+	Course[] courses;
+	
+	/**
+	 * This method runs our analysis logic to get the list of student and course names from lines.
+	 * @param args
+	 */
+	public void run(String[] args) {
+		
+		numOfStudents = Integer.parseInt(args[0]);
+		numOfCourses = Integer.parseInt(args[1]);
+	
+		students = initiateStudentArrayFromLines(lines);
+		
+		System.out.println("Number of All Students: " + numOfStudents);
+		for(Student student: students) {
+			System.out.println(student.getName());
+		}
+		
+		courses = initiateCourseArrayFromLines(lines);
+		System.out.println("Number of All Courses: " + numOfCourses);
+		for(Course course: courses) {
+			System.out.println(course.getCourseName());
+		}
 		
 	}
-	
+
+	/**
+	 * This method returns a Student array to initiate the field, students, from lines.
+	 * @param lines
+	 * @return
+	 */
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
 		
-	}
-	private boolean studentExist(Student[] students, Student student) {
+		// TODO: implement this method
 		
-	}
-	private Course[] initiateCourseArrayFromLines(String[] lines) {
 		
-	}
-	private boolean courseExist(Course[] courses, Course course) {
-		
+		return null;
 	}
 
+	/**
+	 * This method check if there is the same name of the second arugement in the array, students
+	 * @param students
+	 * @param student
+	 * @return boolean
+	 */
+	private boolean studentExist(Student[] students, Student student) {
+		
+		// TODO: implement this method
+
+		return false;
+	}
+	
+	/**
+	 * This method returns a Course array to initiate the field, courses, from lines.
+	 * @param lines
+	 * @return
+	 */
+	private Course[] initiateCourseArrayFromLines(String[] lines) {
+		
+		// TODO: implement this method
+		
+		return null;
+	}
+
+	/**
+	 * This method check if there is the same name of the second argument in the array, courses.
+	 * @param courses
+	 * @param course
+	 * @return boolean
+	 */
+	private boolean courseExist(Course[] courses, Course course) {
+		
+		// TODO: implement this method
+
+		return false;
+	}
 
 }
